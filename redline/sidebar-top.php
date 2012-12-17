3ced404debb20f0adf2344cbbda208fed8726f94
fingli <yul.yordanov@gmail.com>
2012-12-17 17:43:18 +0200

    Add new picture in sidebar-top

--- redline/sidebar-top.php
+++ redline/sidebar-top.php
@@ -7,9 +7,17 @@
 */
 ?>
 <?php if ( ! dynamic_sidebar( 'first-widget-area' ) ) : ?>
+	<!--BEGIN #widget-picture-->
+	<div class="widget">
+		<h3 class="widget-title"><?php _e( 'Picture', 'redline' );?></h3>
+		<img src="<?php echo get_template_directory_uri() .'/library/media/images/Country-Road-with-Balloons_400x250.jpg' ?>" />
+		<p>The image is Public Domain via <a href="http://www.burningwell.org/gallery2/v/Artwork/Country-Road-with-Balloons.jpg.html" target="_blank">burningwell.org</a>.</p>
+	</div><!--END #widget-picture-->	
+	
 	<!--BEGIN #widget-tags-->
 	<div class="widget">
 		<h3 class="widget-title"><?php _e( 'Tags', 'redline' );?></h3>
 		<?php if ( function_exists( 'wp_tag_cloud' ) ) { wp_tag_cloud( 'largest=20' ); } ?>
-	</div><!--END #widget-tags-->
+	</div><!--END #widget-tags-->	
+	
 <?php endif; /* (!function_exists( 'dynamic_sidebar' ) */ ?>
