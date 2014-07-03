c3f743e164a653a25c2eac32f1b5f26bbbc6bc3d
fingli <yul.yordanov@gmail.com>
2014-07-03 12:53:53 +0300

    Change encoding to UTF-8

--- redline/library/media/js/screen.js
+++ redline/library/media/js/screen.js
@@ -22,13 +22,13 @@ jQuery(document).ready(function($) {
 	//	pathClass:		'overideThisToUse',		// the class you have applied to list items that lead to the current page 
 		pathLevels:		1,						// the number of levels of submenus that remain open or are restored using pathClass 
 		delay:			400,					// the delay in milliseconds that the mouse can remain outside a submenu without it closing 
-		animation:		{opacity:'show'},		// an object equivalent to first parameter of jQuery’s .animate() method 
-		speed:			'normal',				// speed of the animation. Equivalent to second parameter of jQuery’s .animate() method 
+		animation:		{opacity:'show'},		// an object equivalent to first parameter of jQuery's .animate() method 
+		speed:			'normal',				// speed of the animation. Equivalent to second parameter of jQuery's .animate() method 
 		autoArrows:		false,					// if true, arrow mark-up generated automatically = cleaner source code at expense of initialisation performance 
 		disableHI:		false					// set to true to disable hoverIntent detection 
-	//	onInit:			function(){},			// callback function fires once Superfish is initialised – 'this' is the containing ul 
-	//	onBeforeShow:	function(){},			// callback function fires just before reveal animation begins – 'this' is the ul about to open 
-	//	onShow:			function(){},			// callback function fires once reveal animation completed – 'this' is the opened ul 
-	//	onHide:			function(){}			// callback function fires after a sub-menu has closed – 'this' is the ul that just closed 
+	//	onInit:			function(){},			// callback function fires once Superfish is initialised - 'this' is the containing ul 
+	//	onBeforeShow:	function(){},			// callback function fires just before reveal animation begins - 'this' is the ul about to open 
+	//	onShow:			function(){},			// callback function fires once reveal animation completed - 'this' is the opened ul 
+	//	onHide:			function(){}			// callback function fires after a sub-menu has closed - 'this' is the ul that just closed 
 	});
 });
